60562652800f44d22e21bf3286d94f8073f1c1d2
Carolina Valdrighi <69602254+cvaldrighi@users.noreply.github.com>
2020-12-08 17:07:44 -0300

    Added some JS funtions
    Appended tasks for the list block;
    Added on click function for line-trought on tasks;
    Added scroll y and some css styles.

--- to_do.js
+++ to_do.js
@@ -1,14 +1,27 @@
 const btnAdd = document.getElementById("btnAdd");
 const input = document.getElementById("input");
+const list = document.getElementById("list");
 
 function addTask() {
      btnAdd.onclick = function() {
         if (input.value == '') {
             alert("please add a task");
         } else {
-            
+            const task = document.createElement("li");
+            list.append(task);
+            task.append(input.value);
+            input.value = '';
         }
     }
 }
 
+function doneTask() {
+    list.onclick = function (e) {
+        const okTask = e.target;
+            okTask.classList.toggle("list__completed");
+    }
+}
+
 addTask();
+doneTask();
+
