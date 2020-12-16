a30874ff47569f1050cce8336f42594423fd639e
Carol Valdrighi <caahrool.valdrighi@gmail.com>
2020-12-16 00:30:02 -0300

    Added enterTask function

--- to_do.js
+++ to_do.js
@@ -2,29 +2,49 @@ const btnAdd = document.getElementById("btnAdd");
 const input = document.getElementById("input");
 const list = document.getElementById("list");
 
+function createTask() {
+    const task = document.createElement("li");
+            const btnX = document.createElement("span");
+            btnX.className = "btn__delete";
+            task.className = "li__task";
+            list.append(task);
+            btnX.append("x");
+            task.append(input.value, btnX);
+            localStorage.setItem("task",input.value);
+            input.value = '';
+            deleteTask();
+            doneTask();
+}
+
 function addTask() {
      btnAdd.onclick = function() {
         if (input.value == '') {
             alert("please add a task");
-        } else {
-            const task = document.createElement("li");
-            const btnX = document.createElement("span");
-            btnX.className = "btn__delete";
-            list.append(task);
-            btnX.append("x");
-            task.append(input.value, btnX);
-            input.value = '';
-            deleteTask();
+        } else { createTask();  
         }
     }
 }
 
-function doneTask() {
-    list.onclick = function (e) {
-        const okTask = e.target;
-            okTask.classList.toggle("list__completed");
-    }
+function enterTask() {
+    input.addEventListener("keydown", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            createTask();
+        }
+    });
 }
+
+function doneTask() {
+    const tasks = document.querySelectorAll(".li__task");
+    tasks.forEach(function(task) {
+        task.onclick = function (e) {
+             const okTask = e.currentTarget;
+             okTask.classList.toggle("list__completed");
+            }
+        })
+    }
+    
+
 
 function deleteTask() {
     const btnDeletes = document.querySelectorAll(".btn__delete");
@@ -37,12 +57,9 @@ function deleteTask() {
             }       
         }
     })
-    
-        
-    
 }
 
 
 addTask();
-doneTask();
+enterTask();
 
