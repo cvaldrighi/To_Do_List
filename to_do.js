43d0b0994b21a1bf993b040dc260b607a0744b7e
Carol Valdrighi <caahrool.valdrighi@gmail.com>
2020-12-18 16:08:00 -0300

    Created Array

--- to_do.js
+++ to_do.js
@@ -1,22 +1,29 @@
 const btnAdd = document.getElementById("btnAdd");
 const input = document.getElementById("input");
 const list = document.getElementById("list");
+const items = JSON.parse(localStorage.getItem("items")) || [];
 
 function createTask() {
     const task = document.createElement("li");
             const btnX = document.createElement("span");
+            const item = {
+                itemtext: this.input.value,
+                done: false
+            };
             btnX.className = "btn__delete";
             task.className = "li__task";
             list.append(task);
             btnX.append("x");
-            task.append(input.value, btnX);
-            localStorage.setItem("task",input.value);
+            task.append(item, btnX);
+            items.push(item);
+            localStorage.setItem("items",JSON.stringify(items));
             input.value = '';
             deleteTask();
             doneTask();
 }
 
 function addTask() {
+     populateItem();
      btnAdd.onclick = function() {
         if (input.value == '') {
             alert("please add a task");
@@ -53,12 +60,24 @@ function deleteTask() {
             const confirmation = confirm("Are you sure?");
             if (confirmation) {
                 const byeTask = e.target;
-                byeTask.parentElement.remove();   
+                byeTask.parentElement.remove();
+
+            
             }       
         }
     })
 }
 
+function populateItem() {
+    list.innerHTML = items.map((item) => {
+        return `
+            <li class="list__item">
+                ${item}
+                <span class="btn__delete">x</span>
+            </li>
+        `;    
+    }).join("");
+}
 
 addTask();
 enterTask();
